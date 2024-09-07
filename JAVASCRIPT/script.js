68e2770dd6b69e179d7e817fb5d5801f1de9a759
INAZ <aynazyq@gmail.com>
2024-09-07 21:39:04 +0330

    Add files via upload

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#input')
+        let x = +input.value
+        function setValue(x) {
+            input.value = x
+        }
+        function readInput() {
+            x = +input.value
+            setValue(x)
+        }
+        function increase(step) {
+            readInput()
+            if(x<10) {
+            x = +input.value
+            x += step
+            setValue(x)}
+
+        }
+        function decrease(step) {
+            readInput()
+            if(x > 1) {
+                x = +input.value
+            x -= step
+            setValue(x) }
+
+        }
